5e7aeb3e4b30937ea0bc14d876f052abf47caa28
Mingwei <s10195172@connect.np.edu.sg>
2022-01-18 12:52:12 +0800

    Login UI (uncompleted) and Firebase Setup

---
+++ ToMatcho/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  ToMatcho
+//
+//  Created by Kwong Ming Wei on 18/1/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ ToMatcho/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  ToMatcho
+//
+//  Created by Kwong Ming Wei on 18/1/22.
+//
+
+import Foundation
